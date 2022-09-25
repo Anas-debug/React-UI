4fb48e35f70d2ffa2cfe8acaa7ff7cac6b4c80a6
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-09-25 14:00:49 +0100

    Installed tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import {BsFillTrashFill} from 'react-icons/bs';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from React</h1>
+      <h1>Trash Icon <BsFillTrashFill /></h1>
     </div>
   );
 }
