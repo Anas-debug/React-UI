514e7bf670d58470a657904153bf6c1b44ab0d5e
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-09-25 15:04:09 +0100

    Tailwind works without adding additional depencies like @craco

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './index.css';
 import {BsFillTrashFill} from 'react-icons/bs';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello from React</h1>
       <h1>Trash Icon <BsFillTrashFill /></h1>
-      <h1 className="text-3xl font-bold underline">Tailwind CSS!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='mx-auto mt-8'>
+        <h1 className='text-4xl text-center'>This is the sidebar</h1>
+    </div>
+  )
+}
